1dff9e82f53c3533e3682a6f817001818b2f39aa
ramildevm <ramil.rahimov2003@gmail.com>
2022-04-05 20:14:36 +0300

    Добавлено окно регистрации

--- ValeriankaApp/RegisterWindow.xaml.cs
+++ ValeriankaApp/RegisterWindow.xaml.cs
@@ -10,33 +10,26 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ValeriankaApp
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для RegisterWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegisterWindow : Window
     {
-        public MainWindow()
+        public RegisterWindow()
         {
             InitializeComponent();
         }
-        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        private void ButtonReg_Click(object sender, RoutedEventArgs e)
         {
-            var register = new RegisterWindow();
-            this.Hide();
-            register.ShowDialog();
-            this.Show();
+            
         }
-
-        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
         {
-            //ClientMainWindow cmw = new ClientMainWindow();
             this.Close();
-            //cmw.ShowDialog();
         }
     }
 }
